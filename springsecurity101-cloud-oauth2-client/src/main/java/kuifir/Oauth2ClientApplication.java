71421f3368c7b24c38f553f27ef74a2ce69cc3cd
kuifir <kuifir@163.com>
2022-07-10 22:33:58 +0800

    feat:添加客户端代码

---
+++ springsecurity101-cloud-oauth2-client/src/main/java/kuifir/Oauth2ClientApplication.java
@@ -0,0 +1,22 @@
+package kuifir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Package: kuifir
+ * <p>
+ * Description： TODO
+ * <p>
+ * Author: baci
+ * <p>
+ * Date: Created in 2022/7/10 21:52
+ * <p>
+ * Version: 0.0.1
+ */
+@SpringBootApplication
+public class Oauth2ClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ClientApplication.class,args);
+    }
+}
---
+++ springsecurity101-cloud-oauth2-client/src/main/java/kuifir/module/config/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package kuifir.module.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Package: kuifir.module.config
+ * <p>
+ * Description： TODO
+ * <p>
+ * Author: baci
+ * <p>
+ * Date: Created in 2022/7/10 21:44
+ * <p>
+ * Version: 0.0.1
+ */
+
+@Configuration
+@Order(200)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    /**
+     * /路径和/login路径允许访问，其它路径需要身份认证后才能访问
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/login**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+}
